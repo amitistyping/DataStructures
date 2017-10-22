48297c473c8738de3e5b04cbd2c15350170e21d1
Jitin Sharma <jitin.social@gmail.com>
2017-10-22 14:03:39 +0530

    I tried
    I tried

---
+++ Stack-Algo/MyLSA.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+
+{
+    int arr[5], s, i;
+
+    cout<<"Enter 5 numbers in this Array.\n";
+    for(i=0;i<5;i++)
+    cin>>arr[i];
+
+cout<<"Enter number to be found.\n";
+cin>>s;
+for(i=0;i<5;i++)
+    if(s==arr[5])
+
+    cout<<"Your number found";
+
+    else
+        cout<<"Your number not found";
+}
