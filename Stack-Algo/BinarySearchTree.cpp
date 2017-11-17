8e4b5674048c3c17e5f13e8a9db87a39da147aac
Amitoj Singh <mail@amitoj.co>
2017-11-17 14:49:05 +0530

    Binary Search Tree Algorithms

--- Stack-Algo/BinarySearchTree.cpp
+++ Stack-Algo/BinarySearchTree.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<conio.h>
+
+//This program creates, traverse, search and insert in a "Binary Search Tree".
+//This pointer (->) has been used to implement this.
+
+struct node //Creating node structure
+{
+    int data;
+    struct node *leftchild; //Points to another structure of same blueprint.
+    struct node *rightchild;
+};
+
 
